ce1a64f843d3d93ceefb0962e01a7b68ce6fb9b8
Jesse White <jesse.white6@gmail.com>
2021-05-10 15:53:51 -0700

    add files and methods for mad libs.
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MadLibs.Models;
+
+namespace MadLibs.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    public ActionResult HomePage() { return View(); }
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+    [Route("/form-aliens")]
+    public ActionResult FormAliens() { return View(); }
+
+    [Route("/madlib")]
+    public ActionResult MadLibResult(string friendName, string actionVerb, string sizeAdjective, string speedAdjective, string throwableObject, string place)
+    {
+      MadLib newMadLib = new MadLib();
+      newMadLib.FriendName = friendName;
+      newMadLib.ActionVerb = actionVerb;
+      newMadLib.SizeAdjective = sizeAdjective;
+      newMadLib.SpeedAdjective = speedAdjective;
+      newMadLib.ThrowableObject = throwableObject;
+      newMadLib.Place = place;
+      return View(newMadLib);
+    }
+    public ActionResult AlienResult(string friendName, string place, string smell, string headShape, string sensation, string vehicle, string soundAdjective)
+    {
+      MadLib newMadLib = new MadLib();
+      newMadLib.FriendName = friendName;
+      newMadLib.Place = place;
+      newMadLib.Smell = smell;
+      newMadLib.HeadShape = headShape;
+      newMadLib.Sensation = sensation;
+      newMadLib.Vehicle = vehicle;
+      newMadLib.SoundsAdjective = soundAdjective;
+      return View(newMadLib);
+    }
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
---
+++ Models/MabLibVariables.cs
@@ -0,0 +1,17 @@
+namespace MadLibs.Models
+{
+  public class MadLib
+  {
+    public string FriendName { get; set; }
+    public string ActionVerb { get; set; }
+    public string SizeAdjective { get; set; }
+    public string SpeedAdjective { get; set; }
+    public string ThrowableObject { get; set; }
+    public string Place { get; set; }
+    public string Smell { get; set; }
+    public string HeadShape { get; set; }
+    public string Sensation { get; set; }
+    public string Vehicle { get; set; }
+    public string SoundsAdjective { get; set; }
+  }
+}
